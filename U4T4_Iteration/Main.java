f256a3300c9ae85d18c274aebfb043f6fa17fefc
brianw3943 <brianw124@nycstudents.net>
2024-11-08 17:28:25 +0000

    commit

---
+++ U4T4_Iteration/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+   public static void main(String[] args)
+   {
+    int result = 0;
+    int k = 0;
+    while (k < 4) {
+      int m = 0;
+      while (m < 3) {  // nested while loop
+        result++;
+        m++;
+        System.out.println("k " + k + ", m " + m);
+      }
+      k++;
+    }
+    System.out.println("result: " + result);
+   }
+}
